82142e5bf934d61e9b13184459c7805a2a8f22b1
zwpdbh <hyperion_z@outlook.com>
2017-03-20 17:26:53 +1300

    <tutorial 3>

---
+++ cmake-build-debug/CMakeFiles/tutorial_02.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tutorial_02.dir/tutorial_02/main.cpp.o"
+  "tutorial_02.pdb"
+  "tutorial_02"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tutorial_02.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ tutorial_02/main.cpp
@@ -0,0 +1,29 @@
+//
+// Created by zwpdbh on 20/03/2017.
+//
+
+#include <iostream>
+#include <opencv2/opencv.hpp>
+using namespace cv;
+using namespace std;
+
+int main( int argc, char** argv )
+{
+    string imageName("../data/HappyFish.jpg");
+    if (argc > 1) {
+        imageName = argv[1];
+    }
+
+    Mat image;
+    image = imread( imageName, 1 );
+
+    Mat gray_image;
+    cvtColor( image, gray_image, COLOR_BGR2GRAY );
+    imwrite( "/Users/zw/Downloads/tmp/Gray_Image.jpg", gray_image );
+    namedWindow( imageName, WINDOW_AUTOSIZE );
+    namedWindow( "Gray image", WINDOW_AUTOSIZE );
+    imshow( imageName, image );
+    imshow( "Gray image", gray_image );
+    waitKey(0);
+    return 0;
+}
